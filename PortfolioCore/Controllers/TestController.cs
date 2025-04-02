0b08a3917591d9332f3a4cd5aa6e058db36e5503
gizem hazal akbulut <gizemhazalakbulut@hotmail.com>
2025-04-02 19:31:48 +0300

    Add project files.

---
+++ PortfolioCore/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.Design.Serialization;
+
+namespace PortfolioCore.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioCore/Entities/Education.cs
@@ -0,0 +1,19 @@
+﻿namespace PortfolioCore.Entities
+{
+    public class Education
+    {
+        //Field (class içinde tanımlanırsa), Variable(method içinde tanımlanırsa), Property(get ve set metotları olan değişken)
+
+        public int EducationId { get; set; }
+
+        public string Title { get; set; }
+
+        public string DateInfo { get; set; }
+
+        public string SubTitle { get; set; }
+
+        public string Description { get; set; }
+    }
+}
+
+
